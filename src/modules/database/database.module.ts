2089f8c59b30d43274d1be3a4e826098ed7af794
Kayê Holanda <kaye.holanda@gmail.com>
2025-03-16 02:08:34 -0300

    feat: adiciona service de usuario

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { databaseProviders } from 'src/providers/database.providers';
+import { databaseProviders } from './database.providers';
 
 @Module({
   providers: [...databaseProviders],
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from "@nestjs/common";
 import { USER_REPOSITORY } from "src/constants";
 import { User } from "./user.entity";
 import { Repository } from "typeorm";
+import { CreateUserDto } from "./dtos/create-user.dto";
 
 @Injectable()
 export class UserService {
@@ -10,8 +11,29 @@ export class UserService {
     private userRepository: Repository<User>,
   ) { }
 
+  async findOne(username: string): Promise<User | null> {
+    return this.userRepository.findOneBy({
+      username
+    });
+  }
+
   async findAll(): Promise<User[]> {
     return this.userRepository.find();
+  }
+
+  async userExists(username: string): Promise<boolean> {
+    return this.userRepository.exists({
+      where: {
+        username
+      }
+    })
+  }
+
+  async create(newUser: CreateUserDto): Promise<User> {
+    // hash user pass
+    const user = this.userRepository.create(newUser);
+
+    return this.userRepository.save(user);
   }
 
 }
